86e91d9774bd41e75982da2c8d2fc92d6222e838
maximilionus <xxxarakxxx@gmail.com>
2020-02-22 13:48:53 +0300

    Added config.ini and finalized it in code

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import msvcrt
-import time
 import sys
 import os
+import time
+import configparser
 from termcolor import colored
 
 if sys.platform == 'win32':
 	os.system('color')
+
+config = configparser.ConfigParser()
+config.read('config.ini')
 
 keypress_time = time.time()
 prev_key = 'none'
@@ -15,7 +19,7 @@ while True:
 		new_keypress_time = time.time()
 		delay_final = new_keypress_time - keypress_time
 		keypress_time = new_keypress_time
-		if key_input == prev_key and delay_final <= 0.15:
+		if key_input == prev_key and delay_final <= float(config['SETTINGS']['MinimalDelay']):
 			print('\n' + colored(f'-> KEY JAM DETECTED ON {key_input} <-', color='white',on_color='on_red') + '\n')
 		else:
 			prev_key = key_input
--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 import sys
 from cx_Freeze import setup, Executable
 
+include_files = ['config.ini']
 # Dependencies are automatically detected, but it might need fine tuning.
-build_exe_options = {"packages": ["os"], "excludes": ["tkinter"]}
+build_exe_options = {"packages": ["os"], "excludes": ["tkinter"], "include_files": include_files}
 
-# GUI applications require a different base on Windows (the default is for a
-# console application).
 base = None
 """ if sys.platform == "win32":
     base = "Win32GUI" """
+
 
 setup(  name = "Jammed Key Detector",
         version = "1.1",
